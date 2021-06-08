b1cd26334d07f1de70423d0f65f05b813b7f61cd
belov <belovdenis777@gmail.com>
2021-06-08 23:48:00 +0300

    start react app - Кинопоиск

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "f9ef934d2bd6b3e4071854f34fbc5fa9";
+
+export const API_KEY_4 =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWVmOTM0ZDJiZDZiM2U0MDcxODU0ZjM0ZmJjNWZhOSIsInN1YiI6IjYwYmZiOTQ5MTNhMzg4MDAyYmE2Nzc0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Tj_hEpMPBWxBnSmfXEld7R6jXt6gbqDKYE5sJCNr9UA";
