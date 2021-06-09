f702e5cafb209e7a416664baefb9683ed85cc2db
belov <belovdenis777@gmail.com>
2021-06-10 00:05:48 +0300

    defaultProps, propTypes создание компонентов Pagination - для страниц,  SortBy для списка сортировки

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SortBy from "./SortBy";
+import Pagination from "./Pagination";
 
 export default class Filters extends React.Component {
     render() {
@@ -9,33 +11,13 @@ export default class Filters extends React.Component {
         } = this.props;
         return (
             <form className="mb-3">
-                <div className="form-group">
-                    <label htmlFor="sort_by">Сортировать по:</label>
-                    <select className="form-control"
-                            value={sort_by}
-                            id='sort_by'
-                            name='sort_by'
-                            onChange={onChangeFilters}
-                    >
-                        <option value="popularity.desc">Популярные по убыванию</option>
-                        <option value="popularity.asc">Популярные по возрастанию</option>
-                        <option value="vote_average.desc">Рейтинг по убыванию</option>
-                        <option value="vote_average.asc">Рейтинг по возрастанию</option>
-                    </select>
-                </div>
-                <div className="btn-group">
-                    <button type="button" className="btn btn-light"
-                            disabled={this.props.page === 1}
-                            onClick={onChangePage.bind(null, this.props.page - 1)
-                            }
-                    >Назад
-                    </button>
-                    <button type="button" className="btn btn-light"
-                            onClick={onChangePage.bind(null, this.props.page + 1)}
-                    >Вперед
-                    </button>
-
-                </div>
+                <SortBy sort_by={sort_by}
+                        onChangeFilters={onChangeFilters}
+                />
+                <Pagination
+                    onChangePage={onChangePage}
+                    page={this.props.page}
+                />
             </form>
         );
     }
